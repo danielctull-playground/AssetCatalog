9dac27e392d238b2ac0d1b27f686318ef1120f97
Daniel Tull <dt@danieltull.co.uk>
2022-07-16 18:03:48 +0100

    Add asset kinds
    These were retrieved from https://developer.apple.com/library/archive/documentation/Xcode/Reference/xcode_ref-Asset_Catalog_Format/AssetTypes.html#//apple_ref/doc/uid/TP40015170-CH30-SW1 and this seems to last have been updated in 2018, so there might be more to add here.

--- Sources/AssetCatalog/Asset.swift
+++ Sources/AssetCatalog/Asset.swift
@@ -3,15 +3,81 @@ import Foundation
 
 public struct Asset: Equatable {
     public let name: String
-    public init(name: String) {
+    public let kind: Kind
+    public init(name: String, kind: Kind) {
         self.name = name
+        self.kind = kind
     }
 }
-
 
 extension Asset {
 
     public init(url: URL) throws {
-        self.init(name: url.filename)
+        try self.init(name: url.filename, kind: Kind(url: url))
+    }
+}
+
+// MARK: - Asset Kind
+
+extension Asset {
+
+    public enum Kind: Equatable {
+        case appIcon
+        case arReferenceImage
+        case arResourceGroup
+        case brandAssets
+        case cubeTexture
+        case dataSet
+        case gameCenterDashboardImageSet
+        case gameCenterLeaderboard
+        case gameCenterLeaderboardSet
+        case iconSet
+        case imageSet
+        case imageStack
+        case imageStackLayer
+        case launchImage
+        case mipmap
+        case namedColor
+        case spriteAtlas
+        case sticker
+        case stickerPack
+        case stickerSequence
+        case texture
+        case watchComplications
+    }
+}
+
+struct UnknownAssetType: Error {
+    let pathExtension: String
+}
+
+extension Asset.Kind {
+
+    init(url: URL) throws {
+        switch url.pathExtension {
+        case "appiconset": self = .appIcon
+        case "arimageset": self = .arReferenceImage
+        case "arresourcegroup": self = .arResourceGroup
+        case "brandassets": self = .brandAssets
+        case "cubetextureset": self = .cubeTexture
+        case "dataset": self = .dataSet
+        case "gcdashboardimage": self = .gameCenterDashboardImageSet
+        case "gcleaderboard": self = .gameCenterLeaderboard
+        case "gcleaderboardset": self = .gameCenterLeaderboardSet
+        case "iconset": self = .iconSet
+        case "imageset": self = .imageSet
+        case "imagestack": self = .imageStack
+        case "imagestacklayer": self = .imageStackLayer
+        case "launchimage": self = .launchImage
+        case "mipmapset": self = .mipmap
+        case "colorset": self = .namedColor
+        case "spriteatlas": self = .spriteAtlas
+        case "sticker": self = .sticker
+        case "stickerpack": self = .stickerPack
+        case "stickersequence": self = .stickerSequence
+        case "textureset": self = .texture
+        case "complicationset": self = .watchComplications
+        default: throw UnknownAssetType(pathExtension: url.pathExtension)
+        }
     }
 }
