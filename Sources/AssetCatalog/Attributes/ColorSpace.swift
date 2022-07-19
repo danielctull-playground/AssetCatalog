786fc028dd3793774eb14e0fe31d3f8d86671c78
Daniel Tull <dt@danieltull.co.uk>
2022-07-19 08:24:06 +0100

    Allow users of the library to specify their own color spaces

--- Sources/AssetCatalog/Attributes/ColorSpace.swift
+++ Sources/AssetCatalog/Attributes/ColorSpace.swift
@@ -1,39 +1,49 @@
 
 public struct ColorSpace: Equatable {
-    fileprivate let value: String
-    public static let sRGB = ColorSpace(value: "srgb")
-    public static let displayP3 = ColorSpace(value: "display-p3")
+    fileprivate let rawValue: String
+    public init(_ rawValue: String) {
+        self.rawValue = rawValue
+    }
+}
+
+extension ColorSpace {
+
+    /// The image uses the standard sRGB color space.
+    public static let sRGB = ColorSpace("srgb")
+
+    /// The image uses a wide gamut color space.
+    public static let displayP3 = ColorSpace("display-p3")
 }
 
 // MARK: - Codable
 
 struct CodableColorSpace {
-    let value: String
+    let rawValue: String
 }
 
 extension CodableColorSpace: Codable {
 
     init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        value = try container.decode(String.self)
+        rawValue = try container.decode(String.self)
     }
 
     func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
-        try container.encode(value)
+        try container.encode(rawValue)
     }
 }
 
 extension ColorSpace {
 
     init(codable: CodableColorSpace) {
-        self.init(value: codable.value)
+        self.init(codable.rawValue)
     }
 }
 
 extension CodableColorSpace {
 
     init(colorSpace: ColorSpace) {
-        self.init(value: colorSpace.value)
+        self.init(rawValue: colorSpace.rawValue)
     }
 }
